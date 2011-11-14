8fd9a934242331e75504b890ae97fe84441322f7
Ran Tavory <rantav@gmail.com>
2011-11-14 15:28:18 +0200

    Add a section for Recent Hosts to the dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -7,13 +7,13 @@ ActiveAdmin::Dashboards.build do
   # == Simple Dashboard Section
   # Here is an example of a simple dashboard section
   #
-  #   section "Recent Posts" do
-  #     ul do
-  #       Post.recent(5).collect do |post|
-  #         li link_to(post.title, admin_post_path(post))
-  #       end
-  #     end
-  #   end
+  section "Recent Hosts" do
+    ul do
+      Host.last(5).collect do |host|
+        li link_to(host.fqdn, admin_host_path(host))
+      end
+    end
+  end
   
   # == Render Partial Section
   # The block is rendered within the context of the view, so you can
