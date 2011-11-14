cad8ab113048eb7aafb533be8a6ff545a2cc73cd
Ran Tavory <rantav@gmail.com>
2011-11-14 13:33:56 +0200

    initial rails project. barebones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onering::Application.config.secret_token = '3bd5dbc56b72568bf9742a31d3171d9d2901a62d286a1549664f68b24d9e4a91385f57a7377924f563cf95267487c437dddd4a9fa585601373acb28f30dafea9'
