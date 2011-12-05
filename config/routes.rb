6f51a8a258c114cdbd96c472ba60cf4c955af8a2
Ran Tavory <rantav@gmail.com>
2011-12-05 11:02:34 +0200

    Remove the hosts sql model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Onering::Application.routes.draw do
-  resources :hosts
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +49,6 @@ Onering::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'hosts#index'
 
   # See how all your routes lay out with "rake routes"
 
