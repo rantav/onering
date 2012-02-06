51a0dae5a816f7def3f0509ac065ec65847f5a0e
Ran Tavory <rantav@gmail.com>
2012-02-06 15:06:49 +0200

    Continue work on the edit UI

--- app/models/datacenter.rb
+++ app/models/datacenter.rb
@@ -3,6 +3,8 @@ class Datacenter
   field :name, :type => String
   has_many :physical_racks
 
+  validates_presence_of :name
+    
   def self.list_options
     Datacenter.all.map {|r| [r.id, r.name]}
   end
--- app/models/physical_rack.rb
+++ app/models/physical_rack.rb
@@ -2,10 +2,13 @@ class PhysicalRack
   include Mongoid::Document
   field :index, :type => Integer
   field :name, :type => String
-  field :dc_name, :type => String
   has_many :physical_hosts
   belongs_to :datacenter
 
+  validates :index, :numericality => { :only_integer => true }   
+  validates_presence_of :name
+  validates_associated :datacenter
+  
   def self.list_options
     PhysicalRack.all.map {|r| [r.id, r.name]}
   end
