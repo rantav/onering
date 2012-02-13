41c1ee9e292c3a712cdbb52b0ccf6ee934954939
Ran Tavory <rantav@gmail.com>
2012-02-13 14:13:01 +0200

    Add Power Consumption to the Physical Host

--- app/models/physical_host.rb
+++ app/models/physical_host.rb
@@ -1,6 +1,7 @@
 class PhysicalHost
   include Mongoid::Document
   include Mongoid::Search
+
   field :name, :type => String
   field :n, :type => Integer
   field :u, :type => Integer
@@ -10,13 +11,16 @@ class PhysicalHost
   field :pdu2, :type => String
   field :notes, :type => String
   field :mac, :type => String
+  field :power_consumption, :type => Float
 
   has_one :parent_host, :class_name => 'PhysicalHost', :inverse_of => :child_hosts
   has_many :child_hosts, :class_name => 'PhysicalHost', :inverse_of => :parent_host
   belongs_to :physical_rack
 
   validates_presence_of :name, :n, :u
-  validates :n, :numericality => { :only_integer => true }   
-  validates :u, :numericality => { :only_integer => true }   
+  validates_numericality_of :n, :only_integer => true
+  validates_numericality_of :u, :only_integer => true
+  validates_numericality_of :power_consumption, :greater_than_or_equal_to => 0
+
   search_in :name, :type, :serial, :pdu1, :pdu2, :notes, :mac, :physical_rack => :name
 end
