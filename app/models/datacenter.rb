b13080bf4eb61d3bac5de186c6bf1b30fb933e06
Ran Tavory <rantav@gmail.com>
2012-02-13 13:14:45 +0200

    Add various properties to the datacenter https://app.asana.com/0/480833545713/480833545757

--- app/models/datacenter.rb
+++ app/models/datacenter.rb
@@ -1,6 +1,15 @@
 class Datacenter
   include Mongoid::Document
+
   field :name, :type => String
+  field :provider_name, :type => String
+  field :location, :type => String
+  field :contact_info, :type => String
+  field :support_link, :type => String
+  field :support_email, :type => String
+  field :support_phone, :type => String
+  
+
   has_many :physical_racks
 
   validates_presence_of :name
