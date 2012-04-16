fdb02eb5040bb3b138085e6d0231f663ffff536d
Ran Tavory <rantav@gmail.com>
2012-04-17 02:29:29 +0300

    Add links to glu from the physical_host view

--- lib/glu_plugin.rb
+++ lib/glu_plugin.rb
@@ -1,10 +1,14 @@
 require 'yaml'
+include ActionView::Helpers::UrlHelper
 
 module GluPlugin
+  @config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'glu.yaml'))).result)
+  def self.link_to_module(module_name)
+    link_to module_name, "#{@config['console_uri']}system/filter?systemFilter=metadata.product%3D%27#{module_name}%27&title=product+%5B#{module_name}%5D&groupBy=metadata.product"
+  end
 
   class Reader
     def initialize
-      @config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'glu.yaml'))).result)
       @glu_api = GluClient::Api.new :rest_uri => @config['rest_uri'], :fabric => @config['fabric']
     end
 
