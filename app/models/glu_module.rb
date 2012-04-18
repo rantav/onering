97d7438d1895467bca024df0c825629551eda347
Ran Tavory <rantav@gmail.com>
2012-04-19 00:44:59 +0300

    Improve the look of the glu labels

--- app/models/glu_module.rb
+++ app/models/glu_module.rb
@@ -6,6 +6,16 @@ class GluModule
   field :state, :type => String
   field :context_base_url => String
   field :version => Integer
-  
+
   embedded_in :physical_host
+
+  def details_str
+    "#{name}: #{state}, v#{version}"
+  end
+
+  def state_label_color
+    return 'label-success' if state == 'running'
+    return 'label-important' if state == 'stopped'
+    return 'label-warning'
+  end
 end
