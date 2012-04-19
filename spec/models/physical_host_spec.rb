d0e9261473c0355e78c216a918b0940ae4d3440b
Ran Tavory <rantav@gmail.com>
2012-04-19 12:57:26 +0300

    Add test for fqdn

--- spec/models/physical_host_spec.rb
+++ spec/models/physical_host_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe PhysicalHost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "fqdn" do
+    it "is correct" do
+      dc = Datacenter.new(:name => 'dc_nane')
+      rack = PhysicalRack.new(:datacenter => dc)
+      PhysicalHost.new(:name => "1", :physical_rack => rack).fqdn.should == "1.dc_nane"
+    end
+  end
 end
