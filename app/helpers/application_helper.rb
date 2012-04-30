57e71fa91ebe91880fce6136623ecfe749280c24
Ran Tavory <rantav@gmail.com>
2012-04-30 11:57:19 +0300

    Move the extra attributes display to a application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def show_extra_attributes(schema, model)
+    html = []
+    if schema
+      schema.entity_attributes.each do |attribute|
+        html.push "<p>"
+        html.push   "<b>#{attribute.name}:</b> "
+        html.push   "#{model[attribute.name]}"
+        html.push "</p>"
+      end
+    end
+    html.join.html_safe
+
+  end
 end
