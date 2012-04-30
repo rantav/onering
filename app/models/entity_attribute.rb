795b6f2b9e710e1a5b9d58f2c5990ed42dbfb012
Ran Tavory <rantav@gmail.com>
2012-04-30 13:08:20 +0300

    Improve the look and feel of the entity_schema

--- app/models/entity_attribute.rb
+++ app/models/entity_attribute.rb
@@ -7,8 +7,12 @@ class EntityAttribute
   field :name, :type => String
   field :type, :type => String
 
+  def self.list_types
+    %w(string)
+  end
+
   validates_presence_of :name
   validates :name, :exclusion => {:in => Mongoid.destructive_fields}
-  validates :type, :inclusion => {:in => %w(string integer)}
+  validates :type, :inclusion => {:in => EntityAttribute.list_types}
 
 end
