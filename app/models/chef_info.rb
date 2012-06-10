5e367e5f417d094d0fb51bb32e579a7298b69f8f
Ran Tavory <rantav@gmail.com>
2012-06-10 14:20:38 +0300

    Unset the attributes in chef_info so that we can get rid of the nil values (bug in mongoid which will only be fixed in 3.0.0)

--- app/models/chef_info.rb
+++ app/models/chef_info.rb
@@ -7,7 +7,10 @@ class ChefInfo
   def apply_json(hash)
     do_not_remove = hash.merge({_id: 1, created_at: 1, updated_at: 1})
     attributes.each do |k,v|
-      remove_attribute(k) unless do_not_remove.include?(k.to_sym) or do_not_remove.include?(k.to_s)
+      unless do_not_remove.include?(k.to_sym) or do_not_remove.include?(k.to_s)
+        remove_attribute(k)
+        unset(k)
+      end
     end
     self.attributes = hash
   end
