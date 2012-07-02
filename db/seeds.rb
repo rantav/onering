a2160e2a14ed26236a68fb71661157fd243aa06c
Ran Tavory <rantav@gmail.com>
2012-07-02 13:06:17 +0300

    Fix typo in the seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 # bootstrap_user = AdminUser.create!(username: 'onering_admin', email: 'onering_admin', password: 'xxx', can_write: true)
 
 ro_api = AdminUser.create!(username: 'ro_api', email: 'ro_api', encrypted_password: 'xxx', can_write: false)
-rw_api = AdminUser.create!(username: 'ro_api', email: 'ro_api', encrypted_password: 'xxx', can_write: true)
+rw_api = AdminUser.create!(username: 'rw_api', email: 'ro_api', encrypted_password: 'xxx', can_write: true)
 settings = Setting.get
 settings.api_ro_username = ro_api.username
 settings.api_ro_password = 'pass'
